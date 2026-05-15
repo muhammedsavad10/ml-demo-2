133bb1a419d4a688236ccbbe4491f7ae6c8adb59
Muhammed Savad T M <muhammedsavadmammu@gmail.com>
2026-05-15 13:52:16 +0530

    Initial commit loaded

---
+++ deploy.py
@@ -0,0 +1,29 @@
+import sagemaker
+from sagemaker.sklearn.models import SKlearnModel
+import time
+import boto3
+
+role="arn:aws:iam::477010600979:role/service-role/AmazonSageMaker-ExecutionRole-20260511T140775"
+
+boto_session=boto3.Session(region_name="eu-north-1")
+session=sagemaker.Session(boto_session=boto_session)
+
+endpoint_name=f"iris-endpoint"--{int(time.time())}
+
+model=SKlearnModel(
+    model_data="s3://my-ml-model-buckets/model.tar.gz",
+    role=role,
+    entry_point="inference.py",
+    framework_version="1.2-1",
+    sagemaker_session=session
+)
+
+sm_client=boto3.client("sagemaker",region_name="eu-north-1")
+
+predictor=model.deploy(
+    initial_instance_count=1,
+    instance_type="ml.m5.large",
+    endpoint_name=endpoint_name
+)
+
+print("Deployment complete")
---
+++ train.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+import os
+
+x,y=load_iris(return_X_y=True)
+model=RandomForestClassifier()
+model.fit(x,y)
+
+os.makedirs("model",exist_ok=True)
+
+with open("model/model.pkl","wb") as f:
+    pickle.dump(model,f)
